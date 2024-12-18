23836bd9ea0846f1c6e026e5f67f888d04bd94aa
m1ndfreak <desychristanti1@gmail.com>
2024-12-18 11:20:10 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,4 @@
-if game.PlaceId == 623823800 then
-    local success, err = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/m1ndfr/lanyard-arsenal/refs/heads/main/main.lua"))()
-    end)
-    
-    if success then
-        print("Cheat loaded successfully for Arsenal.")
-    else
-        warn("Error while loading the cheat: " .. tostring(err))
-    end
-else
-    warn("Game not supported.")
-end
+local UI = require(script.Parent.UI.ui)
+local menu = UI:CreateMenu()
+
+print("UI loaded successfully.")
