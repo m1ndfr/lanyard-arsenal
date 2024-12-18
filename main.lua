b62c2f2ddf220208555d1691f8de546298281bd7
m1ndfreak <desychristanti1@gmail.com>
2024-12-18 11:29:22 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,10 @@
-local UI = require(script.Parent.UI.ui)
-local menu = UI:CreateMenu() 
+local success, UI = pcall(function()
+    return require(script.Parent.UI.ui) -- Load the UI module
+end)
 
-print("UI loaded successfully.")
+if success and UI then
+    local menu = UI:CreateMenu()
+    print("UI loaded successfully.")
+else
+    warn("Failed to load UI module.")
+end
