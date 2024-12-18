7b355539986946c38df0273dcaaea6efe438f6a6
m1ndfreak <desychristanti1@gmail.com>
2024-12-18 11:12:33 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
-
+if game.PlaceId == 286090429 then
+    local UI = require(script.Parent.UI.ui)
+    local menu = UI:CreateMenu()
+    print("Cheat menu loaded successfully for Arsenal.")
+else
+    print("Game not supported.")
+end
